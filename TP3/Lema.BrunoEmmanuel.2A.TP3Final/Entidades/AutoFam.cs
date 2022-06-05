df503f9461857dea7c66f1f5cdac5a05438d9031
BrunoLema <47321553+bruno8877@users.noreply.github.com>
2022-06-05 15:29:40 -0300

    Tp3

---
+++ TP3/Lema.BrunoEmmanuel.2A.TP3Final/Entidades/AutoFam.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class AutoFam : Vehiculo
+    {
+
+        public AutoFam()
+        {
+
+        }
+        
+        public AutoFam(string color, string ruedas, string modelo)
+            :base(color, ruedas, modelo)
+        {
+
+        }
+
+        protected override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"LOS DATOS DEL AUTO SON: {base.Mostrar()}");
+
+            return sb.ToString();
+        }
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Auto && (Auto)obj == this;
+
+        }
+    }
+}
